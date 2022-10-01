a20787b595c22bacc4e7a1ef3fd9f6a2283529b4
Grant Watson <gaw922@gmail.com>
2022-10-01 14:27:16 -0500

    Initial Commit

---
+++ Project11/GameScene.swift
@@ -0,0 +1,48 @@
+//
+//  GameScene.swift
+//  Project11
+//
+//  Created by Grant Watson on 10/1/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 590, y: 410)
+        background.scale(to: CGSize(width: 1180, height: 820))
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 295, y: 0))
+        makeBouncer(at: CGPoint(x: 590, y: 0))
+        makeBouncer(at: CGPoint(x: 885, y: 0))
+        makeBouncer(at: CGPoint(x: 1180, y: 0))
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+    
+    func makeBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+}
